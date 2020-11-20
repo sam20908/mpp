dab8fc4f36c0d53ac3ca3dca8d151054b7d9951e
sam20908 <samestimable2016@gmail.com>
2020-11-19 21:13:31 -0800

    Added TODO for missing test cases

--- test/compile_test/instantiate_non_arithmetic_type/test.cpp
+++ test/compile_test/instantiate_non_arithmetic_type/test.cpp
@@ -6,8 +6,9 @@ struct non_arithmetic_type
 
 int main()
 {
-    matrixpp::matrix<non_arithmetic_type> m;
-    (void) m;
+    matrixpp::matrix<non_arithmetic_type>;
+
+    // @TODO: Add test case for static, dynamic rows, and dynamic columns matrices
 
 	return 0;
 }
