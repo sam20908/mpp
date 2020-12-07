65b84a2c8e8d74426eea853d1cecebe989e0db55
github-action-benchmark <github@users.noreply.github.com>
2020-12-07 03:23:14 +0000

    add Benchmark (googlecpp) benchmark result for e6c8a6640b1b641c6e1ab0bb3e19f96c0ee551f0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607311297732,
+  "lastUpdate": 1607311394099,
   "repoUrl": "https://github.com/sam20908/matrixpp",
   "entries": {
     "Benchmark": [
@@ -66,6 +66,42 @@ window.BENCHMARK_DATA = {
             "value": 28552861.760000497,
             "unit": "ns/iter",
             "extra": "iterations: 25\ncpu: 28550466.24000001 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "samestimable2016@gmail.com",
+            "name": "sam20908",
+            "username": "sam20908"
+          },
+          "committer": {
+            "email": "samestimable2016@gmail.com",
+            "name": "sam20908",
+            "username": "sam20908"
+          },
+          "distinct": true,
+          "id": "e6c8a6640b1b641c6e1ab0bb3e19f96c0ee551f0",
+          "message": "Use same values for determinant test",
+          "timestamp": "2020-12-06T19:20:43-08:00",
+          "tree_id": "99674d48cb0cae1e73d4f2077ff2fd7aa41e73f0",
+          "url": "https://github.com/sam20908/matrixpp/commit/e6c8a6640b1b641c6e1ab0bb3e19f96c0ee551f0"
+        },
+        "date": 1607311392509,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "determinant_5x5",
+            "value": 920.7598571341231,
+            "unit": "ns/iter",
+            "extra": "iterations: 818950\ncpu: 920.6309445021064 ns\nthreads: 1"
+          },
+          {
+            "name": "determinant_10x10",
+            "value": 26761989.407407116,
+            "unit": "ns/iter",
+            "extra": "iterations: 27\ncpu: 26761141.44444444 ns\nthreads: 1"
           }
         ]
       }
