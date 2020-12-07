4081acd2c3a09c2a1db304b80ba479c6f6c1e20c
sam20908 <samestimable2016@gmail.com>
2020-12-06 17:26:05 -0800

    Avoid 100x100 matrix performance test (for now)

--- test/performance_test/determinant/test.cpp
+++ test/performance_test/determinant/test.cpp
@@ -3,28 +3,28 @@
 
 static void determinant_5x5(benchmark::State& state)
 {
-	for (auto _ : state)
-	{
-		matrixpp::matrix a{ { 1000, 1000, 100, 10, 1 },
+	matrixpp::matrix a{ { 1000, 1000, 100, 10, 1 },
 			{ 1000, 1000, 100, 10, 1 },
 			{ 1000, 1000, 100, 10, 1 },
 			{ 1000, 1000, 100, 10, 1 },
 			{ 1000, 1000, 100, 10, 1 } };
 
+	for (auto _ : state)
+	{
 		(void)a.determinant();
 	}
 }
 
 static void determinant_100x100(benchmark::State& state)
 {
+	matrixpp::matrix a(100, 100, 1);
+
 	for (auto _ : state)
 	{
-		matrixpp::matrix a(100, 100, 12345);
-
 		// @TODO: Fix this when determinant API changes
 		(void)a.determinant<long double>();
 	}
 }
 
 BENCHMARK(determinant_5x5);
-BENCHMARK(determinant_100x100);
+// BENCHMARK(determinant_100x100);
