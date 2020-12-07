a81e16ab669318e9fdeb95146089cf72dbab8ca3
github-action-benchmark <github@users.noreply.github.com>
2020-12-07 03:15:31 +0000

    add Benchmark (googlecpp) benchmark result for 97185dca5fd49a784f5a10dce5406ee219d56c43

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1607310931769,
+  "repoUrl": "https://github.com/sam20908/matrixpp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "samestimable2016@gmail.com",
+            "name": "sam20908",
+            "username": "sam20908"
+          },
+          "committer": {
+            "email": "samestimable2016@gmail.com",
+            "name": "sam20908",
+            "username": "sam20908"
+          },
+          "distinct": true,
+          "id": "97185dca5fd49a784f5a10dce5406ee219d56c43",
+          "message": "Fix bench push",
+          "timestamp": "2020-12-06T19:13:47-08:00",
+          "tree_id": "1ba438a939199dfe3d5299d3af92495962112e7b",
+          "url": "https://github.com/sam20908/matrixpp/commit/97185dca5fd49a784f5a10dce5406ee219d56c43"
+        },
+        "date": 1607310931046,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "determinant_5x5",
+            "value": 1080.174335950404,
+            "unit": "ns/iter",
+            "extra": "iterations: 643476\ncpu: 1080.0893832870227 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
