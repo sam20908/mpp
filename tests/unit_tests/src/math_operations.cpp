b4023cdcd59c40f0168a2596c6a9a8bf7a32eed4
sam20908 <samestimable2016@gmail.com>
2021-01-10 16:40:17 -0800

    Improved math operations tests

--- tests/unit_tests/src/math_operations.cpp
+++ tests/unit_tests/src/math_operations.cpp
@@ -21,48 +21,37 @@
 #include <matrixpp/matrix.hpp>
 #include <matrixpp/operation.hpp>
 #include <span>
+#include <stdexcept>
 
 namespace
 {
-	TEST(Operation, Addition_LazyEvaluated)
+	TEST(Operation, Addition_SameSizedOperands)
 	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrix + matrix;
+		auto matrix = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto expr   = matrix + matrix;
 
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
 
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
+		EXPECT_EQ(expr(0, 0), 14);
+		EXPECT_EQ(expr(0, 1), 6);
+		EXPECT_EQ(expr(0, 2), 2);
+		EXPECT_EQ(expr(1, 0), 16);
+		EXPECT_EQ(expr(1, 1), 16);
+		EXPECT_EQ(expr(1, 2), 4);
+		EXPECT_EQ(expr(2, 0), 10);
+		EXPECT_EQ(expr(2, 1), 16);
+		EXPECT_EQ(expr(2, 2), 4);
 	}
 
-	TEST(Operation, Addition_ConstructedMatrix)
+	TEST(Operation, Addition_DifferentSizedOperands_Throw)
 	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix<int, 3, 3>{ matrix + matrix };
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
+		auto matrix1 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 }, { 6, 7, 8 } };
+		auto matrix2 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		EXPECT_THROW((void)(matrix1 + matrix2), std::runtime_error);
 	}
 
-	TEST(Operation, Subtraction_LazyEvaluated)
+	TEST(Operation, Subtraction_SameSizedOperands)
 	{
 		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
 		auto result = matrix - matrix;
@@ -81,213 +70,113 @@ namespace
 		EXPECT_EQ(result(2, 2), 0);
 	}
 
-	TEST(Operation, Subtraction_ConstructedMatrix)
+	TEST(Operation, Subtraction_DifferentSizedOperands_Throw)
+	{
+		auto matrix1 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 }, { 6, 7, 8 } };
+		auto matrix2 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		EXPECT_THROW((void)(matrix1 - matrix2), std::runtime_error);
+	}
+
+	TEST(Operation, Multiplication_MatrixXScalar)
+	{
+		auto matrix = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto expr   = matrix * 2;
+
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
+
+		EXPECT_EQ(expr(0, 0), 14);
+		EXPECT_EQ(expr(0, 1), 6);
+		EXPECT_EQ(expr(0, 2), 2);
+		EXPECT_EQ(expr(1, 0), 16);
+		EXPECT_EQ(expr(1, 1), 16);
+		EXPECT_EQ(expr(1, 2), 4);
+		EXPECT_EQ(expr(2, 0), 10);
+		EXPECT_EQ(expr(2, 1), 16);
+		EXPECT_EQ(expr(2, 2), 4);
+	}
+
+	TEST(Operation, Multiplication_ScalarXMatrix)
+	{
+		auto matrix = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto expr   = 2 * matrix;
+
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
+
+		EXPECT_EQ(expr(0, 0), 14);
+		EXPECT_EQ(expr(0, 1), 6);
+		EXPECT_EQ(expr(0, 2), 2);
+		EXPECT_EQ(expr(1, 0), 16);
+		EXPECT_EQ(expr(1, 1), 16);
+		EXPECT_EQ(expr(1, 2), 4);
+		EXPECT_EQ(expr(2, 0), 10);
+		EXPECT_EQ(expr(2, 1), 16);
+		EXPECT_EQ(expr(2, 2), 4);
+	}
+
+	TEST(Operation, Multiplication_MatrixXMatrix_MultipliableMatrices)
 	{
 		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix<int, 3, 3>{ matrix - matrix };
+		auto expr   = matrix * matrix;
 
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
 
-		EXPECT_EQ(result(0, 0), 0);
-		EXPECT_EQ(result(0, 1), 0);
-		EXPECT_EQ(result(0, 2), 0);
-		EXPECT_EQ(result(1, 0), 0);
-		EXPECT_EQ(result(1, 1), 0);
-		EXPECT_EQ(result(1, 2), 0);
-		EXPECT_EQ(result(2, 0), 0);
-		EXPECT_EQ(result(2, 1), 0);
-		EXPECT_EQ(result(2, 2), 0);
+		EXPECT_EQ(expr(0, 0), 78);
+		EXPECT_EQ(expr(0, 1), 53);
+		EXPECT_EQ(expr(0, 2), 15);
+		EXPECT_EQ(expr(1, 0), 130);
+		EXPECT_EQ(expr(1, 1), 104);
+		EXPECT_EQ(expr(1, 2), 28);
+		EXPECT_EQ(expr(2, 0), 109);
+		EXPECT_EQ(expr(2, 1), 95);
+		EXPECT_EQ(expr(2, 2), 25);
 	}
 
-	TEST(Operation, Multiplication_MatrixXScalar_LazyEvaluated)
+	TEST(Operation, Multiplication_MatrixXMatrix_NotMultipliableMatrices_Throw)
 	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrix * 2;
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
+		auto matrix1 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto matrix2 = matrixpp::matrix<int>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 }, { 1, 1, 1 } };
+		EXPECT_THROW((void)(matrix1 * matrix2), std::runtime_error);
 	}
 
-	TEST(Operation, Multiplication_MatrixXScalar_ConstructedMatrix)
+	TEST(Operation, Division_MatrixXScalar)
 	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix<int, 3, 3>{ matrix * 2 };
+		auto matrix = matrixpp::matrix<float>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto expr   = matrix / 2.F;
 
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
 
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
+		EXPECT_EQ(expr(0, 0), 3.5F);
+		EXPECT_EQ(expr(0, 1), 1.5F);
+		EXPECT_EQ(expr(0, 2), 0.5F);
+		EXPECT_EQ(expr(1, 0), 4.F);
+		EXPECT_EQ(expr(1, 1), 4.F);
+		EXPECT_EQ(expr(1, 2), 1.F);
+		EXPECT_EQ(expr(2, 0), 2.5F);
+		EXPECT_EQ(expr(2, 1), 4.F);
+		EXPECT_EQ(expr(2, 2), 1.F);
 	}
 
-	TEST(Operation, Multiplication_ScalarXMatrix_LazyEvaluated)
+	TEST(Operation, Division_ScalarXMatrix)
 	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = 2 * matrix;
+		auto matrix = matrixpp::matrix<float>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
+		auto expr   = 2.F / matrix;
 
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
+		EXPECT_EQ(expr.rows(), 3);
+		EXPECT_EQ(expr.columns(), 3);
 
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
-	}
-
-	TEST(Operation, Multiplication_ScalarXMatrix_ConstructedMatrix)
-	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix<int, 3, 3>{ 2 * matrix };
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 14);
-		EXPECT_EQ(result(0, 1), 6);
-		EXPECT_EQ(result(0, 2), 2);
-		EXPECT_EQ(result(1, 0), 16);
-		EXPECT_EQ(result(1, 1), 16);
-		EXPECT_EQ(result(1, 2), 4);
-		EXPECT_EQ(result(2, 0), 10);
-		EXPECT_EQ(result(2, 1), 16);
-		EXPECT_EQ(result(2, 2), 4);
-	}
-
-	TEST(Operation, Multiplication_MatrixXMatrix_LazyEvaluated)
-	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrix * matrix;
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 78);
-		EXPECT_EQ(result(0, 1), 53);
-		EXPECT_EQ(result(0, 2), 15);
-		EXPECT_EQ(result(1, 0), 130);
-		EXPECT_EQ(result(1, 1), 104);
-		EXPECT_EQ(result(1, 2), 28);
-		EXPECT_EQ(result(2, 0), 109);
-		EXPECT_EQ(result(2, 1), 95);
-		EXPECT_EQ(result(2, 2), 25);
-	}
-
-	TEST(Operation, Multiplication_MatrixXMatrix_ConstructedMatrix)
-	{
-		auto matrix = matrixpp::matrix<int, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix{ matrix * matrix };
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 78);
-		EXPECT_EQ(result(0, 1), 53);
-		EXPECT_EQ(result(0, 2), 15);
-		EXPECT_EQ(result(1, 0), 130);
-		EXPECT_EQ(result(1, 1), 104);
-		EXPECT_EQ(result(1, 2), 28);
-		EXPECT_EQ(result(2, 0), 109);
-		EXPECT_EQ(result(2, 1), 95);
-		EXPECT_EQ(result(2, 2), 25);
-	}
-
-	TEST(Operation, Division_MatrixXScalar_LazyEvaluated)
-	{
-		auto matrix = matrixpp::matrix<float, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrix / 2.F;
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 3.5F);
-		EXPECT_EQ(result(0, 1), 1.5F);
-		EXPECT_EQ(result(0, 2), 0.5F);
-		EXPECT_EQ(result(1, 0), 4.F);
-		EXPECT_EQ(result(1, 1), 4.F);
-		EXPECT_EQ(result(1, 2), 1.F);
-		EXPECT_EQ(result(2, 0), 2.5F);
-		EXPECT_EQ(result(2, 1), 4.F);
-		EXPECT_EQ(result(2, 2), 1.F);
-	}
-
-	TEST(Operation, Division_MatrixXScalar_ConstructedMatrix)
-	{
-		auto matrix = matrixpp::matrix<float, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix{ matrix / 2.F };
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 3.5F);
-		EXPECT_EQ(result(0, 1), 1.5F);
-		EXPECT_EQ(result(0, 2), 0.5F);
-		EXPECT_EQ(result(1, 0), 4.F);
-		EXPECT_EQ(result(1, 1), 4.F);
-		EXPECT_EQ(result(1, 2), 1.F);
-		EXPECT_EQ(result(2, 0), 2.5F);
-		EXPECT_EQ(result(2, 1), 4.F);
-		EXPECT_EQ(result(2, 2), 1.F);
-	}
-
-	TEST(Operation, Division_ScalarXMatrix_LazyEvaluated)
-	{
-		auto matrix = matrixpp::matrix<float, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = 2.F / matrix;
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 3.5F);
-		EXPECT_EQ(result(0, 1), 1.5F);
-		EXPECT_EQ(result(0, 2), 0.5F);
-		EXPECT_EQ(result(1, 0), 4.F);
-		EXPECT_EQ(result(1, 1), 4.F);
-		EXPECT_EQ(result(1, 2), 1.F);
-		EXPECT_EQ(result(2, 0), 2.5F);
-		EXPECT_EQ(result(2, 1), 4.F);
-		EXPECT_EQ(result(2, 2), 1.F);
-	}
-
-	TEST(Operation, Division_ScalarXMatrix_ConstructedMatrix)
-	{
-		auto matrix = matrixpp::matrix<float, 3, 3>{ { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } };
-		auto result = matrixpp::matrix{ 2.F / matrix };
-
-		EXPECT_EQ(result.rows(), 3);
-		EXPECT_EQ(result.columns(), 3);
-
-		EXPECT_EQ(result(0, 0), 3.5F);
-		EXPECT_EQ(result(0, 1), 1.5F);
-		EXPECT_EQ(result(0, 2), 0.5F);
-		EXPECT_EQ(result(1, 0), 4.F);
-		EXPECT_EQ(result(1, 1), 4.F);
-		EXPECT_EQ(result(1, 2), 1.F);
-		EXPECT_EQ(result(2, 0), 2.5F);
-		EXPECT_EQ(result(2, 1), 4.F);
-		EXPECT_EQ(result(2, 2), 1.F);
+		EXPECT_EQ(expr(0, 0), 3.5F);
+		EXPECT_EQ(expr(0, 1), 1.5F);
+		EXPECT_EQ(expr(0, 2), 0.5F);
+		EXPECT_EQ(expr(1, 0), 4.F);
+		EXPECT_EQ(expr(1, 1), 4.F);
+		EXPECT_EQ(expr(1, 2), 1.F);
+		EXPECT_EQ(expr(2, 0), 2.5F);
+		EXPECT_EQ(expr(2, 1), 4.F);
+		EXPECT_EQ(expr(2, 2), 1.F);
 	}
 
 	TEST(Operation, Addition_PreferStaticExtent)
