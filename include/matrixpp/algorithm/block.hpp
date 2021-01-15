8dc083c7b4cc33e3cc0b53f0ab45c38c9b2d22ad
sam20908 <samestimable2016@gmail.com>
2021-01-15 15:40:56 -0800

    Make block a CPO

--- include/matrixpp/algorithm/block.hpp
+++ include/matrixpp/algorithm/block.hpp
@@ -31,68 +31,80 @@
 
 namespace matrixpp
 {
-	template<typename Value, std::size_t RowsExtent, std::size_t ColumnsExtent>
-	[[nodiscard]] inline decltype(auto) block(const matrix<Value, RowsExtent, ColumnsExtent>& obj,
-		std::size_t top_row_idx,
-		std::size_t top_column_idx,
-		std::size_t bottom_row_idx,
-		std::size_t bottom_column_idx) // @TODO: ISSUE #20
+	struct block_t
 	{
-		// The result matrix is always dynamic because function parameters are always
-		// treated as runtime expressions
-
-		// Out of bounds checks
-
-		auto rows      = obj.rows();
-		auto cols      = obj.columns();
-		auto obj_begin = obj.begin();
-
-		if (top_row_idx >= rows)
+		template<typename Value, std::size_t RowsExtent, std::size_t ColumnsExtent>
+		[[nodiscard]] friend constexpr auto tag_invoke(block_t,
+			const matrix<Value, RowsExtent, ColumnsExtent>& obj,
+			std::size_t top_row_idx,
+			std::size_t top_column_idx,
+			std::size_t bottom_row_idx,
+			std::size_t bottom_column_idx) -> matrix<Value, std::dynamic_extent, std::dynamic_extent>
 		{
-			throw std::invalid_argument("Top row index out of bounds!");
+			// The result matrix is always dynamic because function parameters are always
+			// treated as runtime expressions
+
+			// Out of bounds checks
+
+			auto rows      = obj.rows();
+			auto cols      = obj.columns();
+			auto obj_begin = obj.begin();
+
+			if (top_row_idx >= rows)
+			{
+				throw std::invalid_argument("Top row index out of bounds!");
+			}
+			else if (top_column_idx >= cols)
+			{
+				throw std::invalid_argument("Top column index out of bounds!");
+			}
+			else if (bottom_row_idx >= rows)
+			{
+				throw std::invalid_argument("Bottom row index out of bounds!");
+			}
+			else if (bottom_column_idx >= cols)
+			{
+				throw std::invalid_argument("Bottom column index out of bounds!");
+			}
+
+			// Overlapping checks
+
+			if (top_row_idx > bottom_row_idx)
+			{
+				throw std::invalid_argument("Top row index bigger than bottom row index!");
+			}
+			else if (top_column_idx > bottom_column_idx)
+			{
+				throw std::invalid_argument("Top column index bigger than bottom column index!");
+			}
+
+			auto block_matrix = matrix<Value, std::dynamic_extent, std::dynamic_extent>{};
+			auto block_buf    = std::vector<Value>{};
+			auto block_rows   = bottom_row_idx - top_row_idx + 1;
+			auto block_cols   = bottom_column_idx - top_column_idx + 1;
+
+			block_buf.reserve(block_rows * block_cols);
+
+			for (auto row = top_row_idx; row <= bottom_row_idx; ++row)
+			{
+				auto row_begin_idx = detail::idx_2d_to_1d(cols, row, top_column_idx);
+				auto row_begin     = std::next(obj_begin, row_begin_idx);
+
+				std::ranges::copy_n(row_begin, block_cols, std::back_inserter(block_buf));
+			}
+
+			detail::init_matrix_base_with_1d_rng(block_matrix, block_buf, block_rows, block_cols);
+
+			return block_matrix;
 		}
-		else if (top_column_idx >= cols)
+
+		template<typename... Args>
+		[[nodiscard]] constexpr auto operator()(Args&&... args) const
+			-> detail::tag_invoke_impl::tag_invoke_result_t<block_t, Args...>
 		{
-			throw std::invalid_argument("Top column index out of bounds!");
+			return tag_invoke(*this, std::forward<Args>(args)...);
 		}
-		else if (bottom_row_idx >= rows)
-		{
-			throw std::invalid_argument("Bottom row index out of bounds!");
-		}
-		else if (bottom_column_idx >= cols)
-		{
-			throw std::invalid_argument("Bottom column index out of bounds!");
-		}
+	};
 
-		// Overlapping checks
-
-		if (top_row_idx > bottom_row_idx)
-		{
-			throw std::invalid_argument("Top row index bigger than bottom row index!");
-		}
-		else if (top_column_idx > bottom_column_idx)
-		{
-			throw std::invalid_argument("Top column index bigger than bottom column index!");
-		}
-
-		using block_matrix_t = matrix<Value, std::dynamic_extent, std::dynamic_extent>;
-		auto block_matrix    = block_matrix_t{};
-		auto block_buf       = std::vector<Value>{};
-		auto block_rows      = bottom_row_idx - top_row_idx + 1;
-		auto block_cols      = bottom_column_idx - top_column_idx + 1;
-
-		block_buf.reserve(block_rows * block_cols);
-
-		for (auto row = top_row_idx; row <= bottom_row_idx; ++row)
-		{
-			auto row_begin_idx = detail::idx_2d_to_1d(cols, row, top_column_idx);
-			auto row_begin     = std::next(obj_begin, row_begin_idx);
-
-			std::ranges::copy_n(row_begin, block_cols, std::back_inserter(block_buf));
-		}
-
-		detail::init_matrix_base_with_1d_rng(block_matrix, block_buf, block_rows, block_cols);
-
-		return block_matrix;
-	}
+	inline constexpr auto block = block_t{};
 } // namespace matrixpp
