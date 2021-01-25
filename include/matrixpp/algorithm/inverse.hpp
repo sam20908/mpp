3fe8a92436c40d93fee51a6c91a3a6ebaecd2de7
sam20908 <samestimable2016@gmail.com>
2021-01-25 08:09:12 -0800

    Optimized inverse conditionals

--- include/matrixpp/algorithm/inverse.hpp
+++ include/matrixpp/algorithm/inverse.hpp
@@ -40,93 +40,11 @@ namespace matrixpp
 		{
 			const auto rows = obj.rows();
 			const auto cols = obj.columns();
-			auto det        = lu_decomp_value_t{ 0 };
 
-			// Handle special cases - compute determinant without LU Decomposition
-			if (rows == 0)
-			{
-				det = lu_decomp_value_t{ 1 };
-			}
-			else if (rows == 1)
-			{
-				det = static_cast<lu_decomp_value_t>(obj(0, 0));
-			}
-			else if (rows == 2)
-			{
-				const auto ad = static_cast<lu_decomp_value_t>(obj(0, 0)) * static_cast<lu_decomp_value_t>(obj(1, 1));
-				const auto bc = static_cast<lu_decomp_value_t>(obj(0, 1)) * static_cast<lu_decomp_value_t>(obj(1, 0));
+			using inv_matrix_t = matrix<To, RowsExtent, ColumnsExtent>;
+			auto inv_matrix    = inv_matrix_t{};
 
-				det = ad - bc;
-			}
-
-			// Early singular check for 2x2 and below matrices to avoid computing
-			// LU Decomposition if possible
-			if (rows < 3 && accurate_equals(det, lu_decomp_value_t{ 0 }))
-			{
-				throw std::runtime_error("Inverse of a singular matrix doesn't exist!");
-			}
-
-			using inv_matrix_t       = matrix<To, RowsExtent, ColumnsExtent>;
-			using lu_decomp_matrix_t = matrix<lu_decomp_value_t, RowsExtent, ColumnsExtent>;
-			using lu_decomp_buf_t    = typename lu_decomp_matrix_t::buffer_type;
-			using lu_decomp_diff_t   = typename lu_decomp_matrix_t::difference_type;
-
-			auto inv_matrix = inv_matrix_t{};
-			auto l_buf      = lu_decomp_buf_t{};
-			auto u_buf      = lu_decomp_buf_t{};
-
-			allocate_1d_buf_if_vector(u_buf, rows, cols);
-			std::ranges::copy(obj, u_buf.begin());
-
-			allocate_1d_buf_if_vector(l_buf, rows, cols);
-			transform_1d_buf_into_identity<lu_decomp_value_t>(l_buf, rows);
-
-			// While computing LU Decomposition, we can compute the determinant,
-			// inv(L), and U at the same time
-			// The determinant is needed to evaluate the singularity of 3x3
-			// and bigger matrices
-			det = lu_decomp_value_t{ 1 };
-			for (auto row = std::size_t{ 0 }; row < rows; ++row)
-			{
-				const auto begin_idx = static_cast<lu_decomp_diff_t>(idx_2d_to_1d(cols, row, std::size_t{ 0 }));
-				const auto end_idx   = static_cast<lu_decomp_diff_t>(idx_2d_to_1d(cols, row, cols));
-
-				auto begin = std::next(u_buf.begin(), begin_idx);
-				auto end   = std::next(u_buf.begin(), end_idx);
-
-				for (auto col = std::size_t{ 0 }; col < row; ++col)
-				{
-					// This allows us to keep track of the row of the factor
-					// later on without having to manually calculate from indexes
-					auto factor_row_idx = idx_2d_to_1d(cols, col, col);
-
-					const auto elem_idx = idx_2d_to_1d(cols, row, col);
-					const auto factor   = u_buf[elem_idx] / u_buf[factor_row_idx] * -1;
-
-					std::ranges::transform(begin, end, begin, [factor, factor_row_idx, &u_buf](auto elem) mutable {
-						return factor * u_buf[factor_row_idx++] + elem;
-					});
-
-					// L stores the opposite (opposite sign) of the factors used for
-					// U in the corresponding location, but since inv(A) equals
-					// the opposite sign of the non-pivot (diagnal elements) elements,
-					// we can directly compute the inverse by not changing the sign
-					l_buf[elem_idx] = factor;
-
-					++begin;
-				}
-
-				const auto pivot_idx = idx_2d_to_1d(cols, row, row);
-				det *= u_buf[pivot_idx];
-			}
-
-			if (rows >= 3 && accurate_equals(det, lu_decomp_value_t{ 0 }))
-			{
-				throw std::runtime_error("Inverse of a singular matrix doesn't exist!");
-			}
-
-			// Handle special cases - avoid LU inverse multiplication
-			// and computing inv(U)
+			// Handle special cases - avoid computations
 			if (rows == 0)
 			{
 				// Avoiding creating extra buffer by returning early
@@ -138,27 +56,102 @@ namespace matrixpp
 
 			if (rows == 1)
 			{
-				inv_matrix_buf[0] = To{ 1 } / static_cast<To>(obj(0, 0));
-			}
-			else if (rows == 2)
-			{
-				const auto element_1 = static_cast<To>(obj(1, 1));
-				const auto element_2 = static_cast<To>(obj(0, 1)) * -1;
-				const auto element_3 = static_cast<To>(obj(1, 0)) * -1;
-				const auto element_4 = static_cast<To>(obj(0, 0));
+				const auto elem = static_cast<To>(obj(0, 0));
 
-				inv_matrix_buf[0] = element_1;
-				inv_matrix_buf[1] = element_2;
-				inv_matrix_buf[2] = element_3;
-				inv_matrix_buf[3] = element_4;
+				if (accurate_equals(elem, To{ 0 }))
+				{
+					throw std::runtime_error("Inverse of a singular matrix doesn't exist!");
+				}
 
-				const auto multiplier = To{ 1 } / static_cast<To>(det);
-				std::ranges::transform(inv_matrix_buf,
-					inv_matrix_buf.begin(),
-					std::bind_front(std::multiplies<>{}, multiplier));
+				inv_matrix_buf[0] = To{ 1 } / elem;
 			}
-			else
+
+			auto det = lu_decomp_value_t{ 1 };
+
+			// Early singular check for 2x2 matrices to avoid computations
+			if (rows == 2)
 			{
+				const auto element_1 = static_cast<lu_decomp_value_t>(obj(1, 1));
+				const auto element_2 = static_cast<lu_decomp_value_t>(obj(0, 1));
+				const auto element_3 = static_cast<lu_decomp_value_t>(obj(1, 0));
+				const auto element_4 = static_cast<lu_decomp_value_t>(obj(0, 0));
+
+				const auto ad = element_4 * element_1;
+				const auto bc = element_2 * element_3;
+
+				det = ad - bc;
+
+				if (accurate_equals(det, lu_decomp_value_t{ 0 }))
+				{
+					throw std::runtime_error("Inverse of a singular matrix doesn't exist!");
+				}
+
+				const auto multiplier = lu_decomp_value_t{ 1 } / det;
+
+				inv_matrix_buf[0] = static_cast<To>(multiplier * element_1);
+				inv_matrix_buf[1] = static_cast<To>(multiplier * element_2 * -1);
+				inv_matrix_buf[2] = static_cast<To>(multiplier * element_3 * -1);
+				inv_matrix_buf[3] = static_cast<To>(multiplier * element_4);
+			}
+
+			if (rows >= 3)
+			{
+				using lu_decomp_matrix_t = matrix<lu_decomp_value_t, RowsExtent, ColumnsExtent>;
+				using lu_decomp_buf_t    = typename lu_decomp_matrix_t::buffer_type;
+				using lu_decomp_diff_t   = typename lu_decomp_matrix_t::difference_type;
+
+				auto l_buf = lu_decomp_buf_t{};
+				auto u_buf = lu_decomp_buf_t{};
+
+				allocate_1d_buf_if_vector(u_buf, rows, cols);
+				std::ranges::copy(obj, u_buf.begin());
+
+				allocate_1d_buf_if_vector(l_buf, rows, cols);
+				transform_1d_buf_into_identity<lu_decomp_value_t>(l_buf, rows);
+
+				// While computing LU Decomposition, we can compute the determinant,
+				// inv(L), and U at the same time
+				// The determinant is needed to evaluate the singularity of 3x3
+				// and bigger matrices
+				for (auto row = std::size_t{ 0 }; row < rows; ++row)
+				{
+					const auto begin_idx = static_cast<lu_decomp_diff_t>(idx_2d_to_1d(cols, row, std::size_t{ 0 }));
+					const auto end_idx   = static_cast<lu_decomp_diff_t>(idx_2d_to_1d(cols, row, cols));
+
+					auto begin = std::next(u_buf.begin(), begin_idx);
+					auto end   = std::next(u_buf.begin(), end_idx);
+
+					for (auto col = std::size_t{ 0 }; col < row; ++col)
+					{
+						// This allows us to keep track of the row of the factor
+						// later on without having to manually calculate from indexes
+						auto factor_row_idx = idx_2d_to_1d(cols, col, col);
+
+						const auto elem_idx = idx_2d_to_1d(cols, row, col);
+						const auto factor   = u_buf[elem_idx] / u_buf[factor_row_idx] * -1;
+
+						std::ranges::transform(begin, end, begin, [factor, factor_row_idx, &u_buf](auto elem) mutable {
+							return factor * u_buf[factor_row_idx++] + elem;
+						});
+
+						// L stores the opposite (opposite sign) of the factors used for
+						// U in the corresponding location, but since inv(A) equals
+						// the opposite sign of the non-pivot (diagnal elements) elements,
+						// we can directly compute the inverse by not changing the sign
+						l_buf[elem_idx] = factor;
+
+						++begin;
+					}
+
+					const auto pivot_idx = idx_2d_to_1d(cols, row, row);
+					det *= u_buf[pivot_idx];
+				}
+
+				if (accurate_equals(det, lu_decomp_value_t{ 0 }))
+				{
+					throw std::runtime_error("Inverse of a singular matrix doesn't exist!");
+				}
+
 				// Compute inverse of U directly on the same buffer
 				// Look from the bottom to the top diagnoally for
 				// back-substitution
