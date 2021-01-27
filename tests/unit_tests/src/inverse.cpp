e858adf99d6ef50e433befc1a97cd3587033dcea
sam20908 <samestimable2016@gmail.com>
2021-01-26 19:51:53 -0800

    Added 4x4 inverse test case

--- tests/unit_tests/src/inverse.cpp
+++ tests/unit_tests/src/inverse.cpp
@@ -19,14 +19,14 @@
 
 #include <gtest/gtest.h>
 #include <matrixpp/algorithm/inverse.hpp>
-#include <matrixpp/matrix.hpp>
+#include <matrixpp/matrix/fully_dynamic.hpp>
 #include <utility>
 
 namespace
 {
 	TEST(Inverse, 3x3)
 	{
-		auto matrix  = matrixpp::matrix<int, 3, 3>{ { { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } } };
+		auto matrix  = matrixpp::matrix{ { { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } } };
 		auto inverse = matrixpp::inverse(std::type_identity<float>{}, matrix);
 
 		EXPECT_FLOAT_EQ(inverse(0, 0), 0.F);
@@ -40,24 +40,28 @@ namespace
 		EXPECT_FLOAT_EQ(inverse(2, 2), 16.F / 3.F);
 	}
 
-	// TEST(Inverse, 4x4)
-	// {
-	// 	// auto matrix = matrixpp::matrix<int, 3, 3>{ { { 7, 3, 1 }, { 8, 8, 2 }, { 5, 8, 2 } } };
-	// 	auto matrix =
-	// 		matrixpp::matrix<int, 4, 4>{ { { 2, 4, 6, 9 }, { 0, -1, -8, 1 }, { 0, 0, 96, 4 }, { 0, 0, 96, 5 } } };
-	// 	auto inverse = matrixpp::inverse(std::type_identity<float>{}, matrix);
-	// 	(void)inverse;
+	TEST(Inverse, 4x4)
+	{
+		auto matrix  = matrixpp::matrix{ { { 2, 4, 6, 9 }, { 0, -1, -8, 1 }, { 0, 0, 96, 4 }, { 0, 0, 96, 5 } } };
+		auto inverse = matrixpp::inverse(std::type_identity<float>{}, matrix);
 
-	// 	// EXPECT_FLOAT_EQ(inverse(0, 0), 0.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(0, 1), 1.F / 3.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(0, 2), -1.F / 3.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(1, 0), -1.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(1, 1), 3.F / 2.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(1, 2), -1.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(2, 0), 4.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(2, 1), -41.F / 6.F);
-	// 	// EXPECT_FLOAT_EQ(inverse(2, 2), 16.F / 3.F);
-	// }
+		EXPECT_FLOAT_EQ(inverse(0, 0), 1.F / 2.F);
+		EXPECT_FLOAT_EQ(inverse(0, 1), 2.F);
+		EXPECT_FLOAT_EQ(inverse(0, 2), 689.F / 96.F);
+		EXPECT_FLOAT_EQ(inverse(0, 3), -169.F / 24.F);
+		EXPECT_FLOAT_EQ(inverse(1, 0), 0.F);
+		EXPECT_FLOAT_EQ(inverse(1, 1), -1.F);
+		EXPECT_FLOAT_EQ(inverse(1, 2), -17.F / 12.F);
+		EXPECT_FLOAT_EQ(inverse(1, 3), 4.F / 3.F);
+		EXPECT_FLOAT_EQ(inverse(2, 0), 0.F);
+		EXPECT_FLOAT_EQ(inverse(2, 1), 0.F);
+		EXPECT_FLOAT_EQ(inverse(2, 2), 5.F / 96.F);
+		EXPECT_FLOAT_EQ(inverse(2, 3), -1.F / 24.F);
+		EXPECT_FLOAT_EQ(inverse(3, 0), 0.F);
+		EXPECT_FLOAT_EQ(inverse(3, 1), 0.F);
+		EXPECT_FLOAT_EQ(inverse(3, 2), -1.F);
+		EXPECT_FLOAT_EQ(inverse(3, 3), 1.F);
+	}
 
 	// TEST(Inverse, 5x5)
 	// {
