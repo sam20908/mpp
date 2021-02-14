99ce490a06c6c92a3d6fc9e5dff154fe4740180c
sam20908 <samestimable2016@gmail.com>
2021-02-14 10:08:13 -0800

    Enhanced output of pylint wrapper

--- tools/pylint_recursive.py
+++ tools/pylint_recursive.py
@@ -1,24 +1,47 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 '''
 Module that runs pylint on all python scripts found in a directory tree..
 
 Originally from https://gist.github.com/MJJoyce/5835693. Edited with the following changes:
 - Adapted to mpp project's pylintrc
-- Only scan for .py files in whitelisted directories
+- Only scan for .py files in whitelisted directories (relative to the root directory)
 - Display which files failed to meet required score and exit with non-zero code
+- Fixed missing space in "Using config file at" and the configuration filename
+- Added color output
+- Don't print previous score because we are not using presistent comparison
+- Changed shebang to python3
 '''
+
+# pylint: disable=C0301
 
 import os
 import re
 import sys
 import pathlib
+import colorama
 
 TOTAL = 0.0
 COUNT = 0
 
-WHITELISTED_DIRS = ["benchmarks", "tests"]
+WHITELISTED_DIRS = ["benchmarks", "tests", "tools"]
 FAILED_MODULES = []
+
+CATEGORY_COLORS = {"E": colorama.Fore.LIGHTRED_EX, "F": colorama.Fore.RED,
+                   "W": colorama.Fore.YELLOW, "C": colorama.Fore.CYAN,
+                   "R": colorama.Fore.LIGHTYELLOW_EX,
+                   "I": colorama.Style.DIM}
+
+
+def check_if_module_is_skipped(module):
+    """ Checks if module will be skipped by pylint """
+    with open(module, "r") as reader:
+        for line in reader:
+            # Make sure to not skip itself if it's not marked with the appropriate annotation
+            if "# pylint: skip-file" in line and not module == "tools/pylint_recursive.py":
+                return True
+
+    return False
 
 
 def check(cfg_dir, module):
@@ -30,49 +53,74 @@ def check(cfg_dir, module):
     if module.endswith(".py"):
 
         print("CHECKING " + module)
-        pout = os.popen("pylint %s %s" % (cfg_dir, module), "r")
+        print("--" * 50)
+
+        # Since pylint does not give any output regarding skipped modules, check them manually
+        is_skipped = check_if_module_is_skipped(module)
+        if is_skipped:
+            print(
+                f"{module} is {colorama.Fore.MAGENTA}skipped by Pylint{colorama.Style.RESET_ALL}\n")
+            return
+
+        current_module_failed = False
+
+        pout = os.popen(f"pylint {cfg_dir} {module}", "r")
         for line in pout:
-            # NOTE (bugfix): Don't use match because that only matches the beginning of the string
-            if re.search(r"E....:", line):
-                print(line)
-                FAILED_MODULES.append(module)
-
             if "Your code has been rated at" in line:
-                print(line)
-
                 # Since we disabled persistent comparison, grab the current score
                 score = re.findall(r"\d+.\d\d", line)[0]
+                newline_at_beginning = "\n" if current_module_failed else ""
                 TOTAL += float(score)
                 COUNT += 1
 
+                print(
+                    f"{newline_at_beginning}Your code has been rated at {colorama.Fore.GREEN}{score}{colorama.Style.RESET_ALL}/10\n")
+
+                if current_module_failed:
+                    FAILED_MODULES.append(module)
+            else:
+                # NOTE (bugfix): Don't use match because that only matches the beginning of the string
+                groups = re.search(
+                    r"^(.*:\d+:\d+:) ([A-Z]+)(\d+): (.*)", line)
+
+                if not groups is None:
+                    fault_at_module_location = groups.group(1)
+                    category = groups.group(2)
+                    code_num = groups.group(3)
+                    message = groups.group(4)
+                    color = CATEGORY_COLORS[category]
+
+                    print(
+                        f"{fault_at_module_location} {color}{category}{code_num}: {message}{colorama.Style.RESET_ALL}")
+
+                    current_module_failed = True
+
 
 if __name__ == "__main__":
-    try:
-        BASE_DIRECTORY = sys.argv[1]
-        print("Setting base directory to" + BASE_DIRECTORY)
-    except IndexError:
-        BASE_DIRECTORY = os.getcwd()
-        print("No base directory passed. Defaulting to current directory...")
+    colorama.init()
 
     try:
-        CONFIG_DIRECTORY = "--rcfile=" + sys.argv[2]
-        print("Using config file at" + sys.argv[2])
+        CONFIG_DIRECTORY = "--rcfile=" + sys.argv[1]
+        print("Using config file at " + sys.argv[1])
     except IndexError:
         CONFIG_DIRECTORY = ""
         print("No config file passed. Defaulting to PEP8...")
 
-    print("Looking for *.py files in subdirectories of " + BASE_DIRECTORY)
+    print("Getting *.py files in whitelisted directories")
+    print("Whitelisted directories are: " + ", ".join(WHITELISTED_DIRS))
+    print("")
 
     for whitelisted_dir in WHITELISTED_DIRS:
         for file in pathlib.Path(whitelisted_dir).rglob("*.py"):
             check(CONFIG_DIRECTORY, str(file))
 
     print("==" * 50)
-    print("%d modules found" % COUNT)
-    print("AVERAGE SCORE = %.02f" % (TOTAL / COUNT))
+    print("%d modules checked" % COUNT)
+    print(
+        f"AVERAGE SCORE = {colorama.Fore.GREEN}%.02f{colorama.Style.RESET_ALL}/10" % (TOTAL / COUNT))
 
     if len(FAILED_MODULES) > 0:
         print("==" * 50)
-        print("The following modules needs to be fixed:\n")
+        print("The following files needs to be fixed:\n")
         print(*FAILED_MODULES, sep="\n")
         sys.exit(1)
