191486836656d43ff878b9d931261fa05deec2d7
sam20908 <samestimable2016@gmail.com>
2021-02-21 20:14:44 -0800

    Fixed formatting of website scripts
    ***NO_CI***

--- external/website/src/ts/CodeBlock.tsx
+++ external/website/src/ts/CodeBlock.tsx
@@ -3,60 +3,57 @@ import styled from "styled-components";
 import Highlight, { defaultProps, PrismTheme } from "prism-react-renderer";
 
 const Pre = styled.pre`
-	text-align: left;
-	margin: 1em 0;
-	padding: 0.5em;
-	overflow: scroll;
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow: scroll;
 
-	& .token-line {
-		line-height: 1.3em;
-		height: 1.3em;
-	}
+  & .token-line {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
 `;
 
 const Line = styled.div`
-	display: table-row;
+  display: table-row;
 `;
 
 const LineNo = styled.span`
-	display: table-cell;
-	text-align: right;
-	padding-right: 1em;
-	user-select: none;
-	opacity: 0.5;
+  display: table-cell;
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
 `;
 
 const LineContent = styled.span`
-	display: table-cell;
+  display: table-cell;
 `;
 
 interface Props {
-	theme: PrismTheme;
-	code: string;
+  theme: PrismTheme;
+  code: string;
 }
 
 const CodeBlock: React.FunctionComponent<Props> = ({ theme, code }: Props) => {
-	return (
-		<Highlight {...defaultProps} theme={theme} code={code} language="cpp">
-			{({ className, style, tokens, getLineProps, getTokenProps }) => (
-				<Pre className={className} style={style}>
-					{tokens.map((line, i) => (
-						<Line key={i} {...getLineProps({ line, key: i })}>
-							<LineNo>{i + 1}</LineNo>
-							<LineContent>
-								{line.map((token, key) => (
-									<span
-										key={key}
-										{...getTokenProps({ token, key })}
-									/>
-								))}
-							</LineContent>
-						</Line>
-					))}
-				</Pre>
-			)}
-		</Highlight>
-	);
+  return (
+    <Highlight {...defaultProps} theme={theme} code={code} language="cpp">
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <Line key={i} {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
+              <LineContent>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </LineContent>
+            </Line>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  );
 };
 
 export default CodeBlock;
