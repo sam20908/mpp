d9e02111a89a5679bb688863ff581022402d80ef
sam20908 <samestimable2016@gmail.com>
2021-03-13 08:50:21 -0800

    Move comparison CPOs out of detail namespace

--- include/mpp/utility/comparison.hpp
+++ include/mpp/utility/comparison.hpp
@@ -31,73 +31,69 @@
 
 namespace mpp
 {
-	namespace detail
+	struct size_compare_t : public detail::cpo_base<size_compare_t>
 	{
-		struct size_compare_t : public cpo_base<size_compare_t>
+		template<typename LeftValue,
+			typename RightValue,
+			std::size_t LeftRowsExtent,
+			std::size_t LeftColumnsExtent,
+			std::size_t RightRowsExtent,
+			std::size_t RightColumnsExtent>
+		[[nodiscard]] friend inline auto tag_invoke(size_compare_t,
+			const matrix<LeftValue, LeftRowsExtent, LeftColumnsExtent>& left,
+			const matrix<RightValue, RightRowsExtent, RightColumnsExtent>& right,
+			bool compare_rows,
+			bool compare_columns) -> std::pair<std::partial_ordering, std::partial_ordering> // @TODO: ISSUE #20
 		{
-			template<typename LeftValue,
-				typename RightValue,
-				std::size_t LeftRowsExtent,
-				std::size_t LeftColumnsExtent,
-				std::size_t RightRowsExtent,
-				std::size_t RightColumnsExtent>
-			[[nodiscard]] friend inline auto tag_invoke(size_compare_t,
-				const matrix<LeftValue, LeftRowsExtent, LeftColumnsExtent>& left,
-				const matrix<RightValue, RightRowsExtent, RightColumnsExtent>& right,
-				bool compare_rows,
-				bool compare_columns) -> std::pair<std::partial_ordering, std::partial_ordering> // @TODO: ISSUE #20
-			{
-				return std::pair{ compare_rows ? left.rows() <=> right.rows() : std::partial_ordering::unordered,
-					compare_columns ? left.columns() <=> right.columns() : std::partial_ordering::unordered };
-			}
-		};
+			return std::pair{ compare_rows ? left.rows() <=> right.rows() : std::partial_ordering::unordered,
+				compare_columns ? left.columns() <=> right.columns() : std::partial_ordering::unordered };
+		}
+	};
 
-		struct elements_compare_t : public cpo_base<elements_compare_t>
+	struct elements_compare_t : public detail::cpo_base<elements_compare_t>
+	{
+		template<typename Value,
+			std::size_t LeftRowsExtent,
+			std::size_t LeftColumnsExtent,
+			std::size_t RightRowsExtent,
+			std::size_t RightColumnsExtent,
+			typename CompareThreeway = std::compare_three_way>
+		[[nodiscard]] friend inline auto tag_invoke(elements_compare_t,
+			const matrix<Value, LeftRowsExtent, LeftColumnsExtent>& left,
+			const matrix<Value, RightRowsExtent, RightColumnsExtent>& right,
+			CompareThreeway compare_three_way_fn = {}) // @TODO: ISSUE #20
 		{
-			template<typename Value,
-				std::size_t LeftRowsExtent,
-				std::size_t LeftColumnsExtent,
-				std::size_t RightRowsExtent,
-				std::size_t RightColumnsExtent,
-				typename CompareThreeway = std::compare_three_way>
-			[[nodiscard]] friend inline auto tag_invoke(elements_compare_t,
-				const matrix<Value, LeftRowsExtent, LeftColumnsExtent>& left,
-				const matrix<Value, RightRowsExtent, RightColumnsExtent>& right,
-				CompareThreeway compare_three_way_fn = {}) // @TODO: ISSUE #20
+			const auto left_size  = left.size();
+			const auto right_size = right.size();
+
+			using ordering_type = std::compare_three_way_result_t<Value, Value>;
+
+			if (left_size != right_size)
 			{
-				const auto left_size  = left.size();
-				const auto right_size = right.size();
-
-				using ordering_type = std::compare_three_way_result_t<Value, Value>;
-
-				if (left_size != right_size)
-				{
-					return ordering_type{ left_size <=> right_size };
-				}
-
-				// We can't just use <=> because of two different types of matrices to compare
-
-				const auto end = left.cend();
-
-				auto left_it  = left.cbegin();
-				auto right_it = right.cbegin();
-
-				for (; left_it != end; ++left_it, ++right_it)
-				{
-					const auto ordering = compare_three_way_fn(*left_it, *right_it);
-
-					if (ordering != 0)
-					{
-						return ordering;
-					}
-				}
-
-				return ordering_type::equivalent;
+				return ordering_type{ left_size <=> right_size };
 			}
-		};
 
-	} // namespace detail
+			// We can't just use <=> because of two different types of matrices to compare
 
-	inline constexpr auto size_compare     = detail::size_compare_t{};
-	inline constexpr auto elements_compare = detail::elements_compare_t{};
+			const auto end = left.cend();
+
+			auto left_it  = left.cbegin();
+			auto right_it = right.cbegin();
+
+			for (; left_it != end; ++left_it, ++right_it)
+			{
+				const auto ordering = compare_three_way_fn(*left_it, *right_it);
+
+				if (ordering != 0)
+				{
+					return ordering;
+				}
+			}
+
+			return ordering_type::equivalent;
+		}
+	};
+
+	inline constexpr auto size_compare     = size_compare_t{};
+	inline constexpr auto elements_compare = elements_compare_t{};
 } // namespace mpp
