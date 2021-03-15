260c3fb4be2bb3c10642a66451056450adfec29f
sam20908 <samestimable2016@gmail.com>
2021-03-15 09:17:45 -0700

    Fixed formatting

--- external/tests/src/algorithm_cpos.cpp
+++ external/tests/src/algorithm_cpos.cpp
@@ -82,8 +82,7 @@ void test_det(const std::string& filename, Tag test_tag = tag("execute"))
 
 		using ordering_type = std::compare_three_way_result_t<value_type, value_type>;
 
-		expect(
-			mpp::floating_point_compare(det, static_cast<value_type>(result)) == ordering_type::equivalent);
+		expect(mpp::floating_point_compare(det, static_cast<value_type>(result)) == ordering_type::equivalent);
 	};
 }
 
@@ -104,8 +103,7 @@ void test_transformation_helper(const auto& transform_fn, const auto& result, co
 
 	using ordering_type = std::compare_three_way_result_t<value_type, value_type>;
 
-	expect(mpp::elements_compare(result_matrix, transformed, mpp::floating_point_compare) ==
-		   ordering_type::equivalent);
+	expect(mpp::elements_compare(result_matrix, transformed, mpp::floating_point_compare) == ordering_type::equivalent);
 }
 
 template<std::size_t RowsExtent,
@@ -153,8 +151,7 @@ void test_block_helper(const auto& results, const auto& data, auto... dimension_
 
 		using ordering_type = std::compare_three_way_result_t<value_type, value_type>;
 
-		expect(mpp::elements_compare(result_matrix, cropped, mpp::floating_point_compare) ==
-			   ordering_type::equivalent);
+		expect(mpp::elements_compare(result_matrix, cropped, mpp::floating_point_compare) == ordering_type::equivalent);
 	}
 }
 
