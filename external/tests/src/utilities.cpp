1188e2dace2fff4d40612012ae2e49772d206afd
Sam Huang <samestimable2016@gmail.com>
2021-06-30 12:17:06 -0700

    Revamped utilities test!

--- external/tests/src/utilities.cpp
+++ external/tests/src/utilities.cpp
@@ -22,8 +22,7 @@
 #include <mpp/matrix.hpp>
 #include <mpp/utility.hpp>
 
-#include "../../include/custom_allocator.hpp"
-#include "../../include/test_parsers.hpp"
+#include "../../include/test_utilities.hpp"
 
 #include <compare>
 #include <concepts>
@@ -32,188 +31,127 @@
 
 using namespace boost::ut::bdd;
 using namespace boost::ut;
+using namespace mpp;
 
-inline auto stringify_type = [](mpp::matrix_type mat_type) {
-	switch (mat_type)
-	{
-	case mpp::matrix_type::fully_static:
-		return "matrix_type::fully_static";
-		break;
-	case mpp::matrix_type::fully_dynamic:
-		return "matrix_type::fully_dynamic";
-		break;
-	case mpp::matrix_type::dynamic_rows:
-		return "matrix_type::dynamic_rows";
-		break;
-	case mpp::matrix_type::dynamic_columns:
-		return "matrix_type::dynamic_columns";
-		break;
-		[[unlikely]] default : return "unknown";
-	}
-};
-
-inline auto stringify_ordering = [](auto ordering) {
-	using ordering_t = std::remove_cvref_t<decltype(ordering)>;
-
-	if constexpr (std::is_same_v<ordering_t, std::strong_ordering>)
-	{
-		if (ordering == std::strong_ordering::greater)
-		{
-			return "strong_ordering::greater";
-		}
-		else if (ordering == std::strong_ordering::less)
-		{
-			return "strong_ordering::less";
-		}
-		else if (ordering == std::strong_ordering::equivalent)
-		{
-			return "strong_ordering::equivalent";
-		}
-		else
-		{
-			return "unknown";
-		}
-	}
-	else if constexpr (std::is_same_v<ordering_t, std::weak_ordering>)
-	{
-		if (ordering == std::weak_ordering::greater)
-		{
-			return "weak_ordering::greater";
-		}
-		else if (ordering == std::weak_ordering::less)
-		{
-			return "weak_ordering::less";
-		}
-		else if (ordering == std::weak_ordering::equivalent)
-		{
-			return "weak_ordering::equivalent";
-		}
-		else
-		{
-			return "";
-		}
-	}
-	else if constexpr (std::is_same_v<ordering_t, std::partial_ordering>)
-	{
-		if (ordering == std::partial_ordering::greater)
-		{
-			return "partial_ordering::greater";
-		}
-		else if (ordering == std::partial_ordering::less)
-		{
-			return "partial_ordering::less";
-		}
-		else if (ordering == std::partial_ordering::equivalent)
-		{
-			return "partial_ordering::equivalent";
-		}
-		else if (ordering == std::partial_ordering::unordered)
-		{
-			return "partial_ordering::unordered";
-		}
-		else
-		{
-			return "unknown";
-		}
-	}
-	else
-	{
-		return "unknown";
-	}
-};
-
-inline auto stringify_noop = [](const auto& arg) {
-	return arg;
-};
-
-template<typename Mat, typename Val, typename Fn, typename StringifyFn>
-void test_fn(std::string_view filename, const Fn& fn, const StringifyFn& stringify_fn)
+namespace
 {
-	test(filename.data()) = [=]() {
-		const auto [mat, expected_val] = parse_test(filename, parse_mat<Mat>(), parse_val<Val>);
-		const auto out                 = fn(mat);
+	template<typename Mats>
+	void test_fn(std::string_view test_name, const auto& fn, const auto& parse_fn)
+	{
+		test(test_name.data()) =
+			[&, test_name]<typename T, std::size_t RowsExtent, std::size_t ColumnsExtent, typename Alloc>(
+				std::type_identity<matrix<T, RowsExtent, ColumnsExtent, Alloc>>) {
+				using mat_t = matrix<T, RowsExtent, ColumnsExtent, Alloc>;
 
-		expect(out == expected_val) << "Output is" << stringify_fn(out) << "but expected output is"
-									<< stringify_fn(expected_val);
-	};
-}
+				const auto [mat, expected_val] = parse_test(test_name, parse_mat<mat_t>, parse_fn);
+				const auto out                 = fn(mat);
 
-template<typename Mat, typename Mat2>
-void test_cmp_size(std::string_view filename)
-{
-	test(filename.data()) = [=]() {
-		const auto [mat, mat2, cmp_rows, cmp_cols, expected_row_ordering, expected_col_ordering] = parse_test(filename,
-			parse_mat<Mat>(),
-			parse_mat<Mat2>(),
-			parse_val<bool>,
-			parse_val<bool>,
-			parse_ordering<std::partial_ordering>,
-			parse_ordering<std::partial_ordering>);
+				expect(out == expected_val);
+			} |
+			Mats{};
+	}
 
-		const auto [out_row_ordering, out_col_ordering] = mpp::size_compare(mat, mat2, cmp_rows, cmp_cols);
+	template<typename Mats, typename Order, typename Order2>
+	void test_cmp_size(std::string_view test_name)
+	{
+		test(test_name.data()) = [&, test_name]<typename T,
+									 typename T2,
+									 std::size_t RowsExtent,
+									 std::size_t RowsExtent2,
+									 std::size_t ColumnsExtent,
+									 std::size_t ColumnsExtent2,
+									 typename Alloc,
+									 typename Alloc2>(
+									 std::tuple<std::type_identity<matrix<T, RowsExtent, ColumnsExtent, Alloc>>,
+										 std::type_identity<matrix<T2, RowsExtent2, ColumnsExtent2, Alloc2>>>) {
+			using mat_t  = matrix<T, RowsExtent, ColumnsExtent, Alloc>;
+			using mat2_t = matrix<T2, RowsExtent2, ColumnsExtent2, Alloc2>;
 
-		expect(out_row_ordering == expected_row_ordering)
-			<< "Output is" << stringify_ordering(out_row_ordering) << "but expected output is"
-			<< stringify_ordering(expected_row_ordering);
-		expect(out_col_ordering == expected_col_ordering)
-			<< "Output is" << stringify_ordering(out_col_ordering) << "but expected output is"
-			<< stringify_ordering(expected_col_ordering);
-	};
-}
+			const auto [mat, mat2, cmp_rows, cmp_cols, expected_row_cmp, expected_col_cmp] = parse_test(test_name,
+				parse_mat<mat_t>,
+				parse_mat<mat2_t>,
+				parse_val<bool>,
+				parse_val<bool>,
+				parse_ordering<Order>,
+				parse_ordering<Order2>);
 
-template<typename Mat, typename Mat2, typename Ordering>
-void test_cmp_elems(std::string_view filename)
-{
-	test(filename.data()) = [=]() {
-		const auto [mat, mat2, expected_ordering] =
-			parse_test(filename, parse_mat<Mat>(), parse_mat<Mat2>(), parse_ordering<Ordering>);
+			const auto out = size_compare(mat, mat2, cmp_rows, cmp_cols);
 
-		const auto out_ordering = mpp::elements_compare(mat, mat2, mpp::floating_point_compare);
+			expect(out.first == expected_row_cmp);
+			expect(out.second == expected_col_cmp);
+		} | Mats{};
+	}
 
-		expect(out_ordering == expected_ordering) << "Output is" << stringify_ordering(out_ordering)
-												  << "but expected output is" << stringify_ordering(expected_ordering);
-	};
-}
+	template<typename Mats, typename Order>
+	void test_cmp_elems(std::string_view test_name)
+	{
+		test(test_name.data()) = [&, test_name]<typename T,
+									 typename T2,
+									 std::size_t RowsExtent,
+									 std::size_t RowsExtent2,
+									 std::size_t ColumnsExtent,
+									 std::size_t ColumnsExtent2,
+									 typename Alloc,
+									 typename Alloc2>(
+									 std::tuple<std::type_identity<matrix<T, RowsExtent, ColumnsExtent, Alloc>>,
+										 std::type_identity<matrix<T2, RowsExtent2, ColumnsExtent2, Alloc2>>>) {
+			using mat_t  = matrix<T, RowsExtent, ColumnsExtent, Alloc>;
+			using mat2_t = matrix<T2, RowsExtent2, ColumnsExtent2, Alloc2>;
+
+			const auto [mat, mat2, expected_cmp] =
+				parse_test(test_name, parse_mat<mat_t>, parse_mat<mat2_t>, parse_ordering<Order>);
+
+			const auto out = elements_compare(mat, mat2, floating_point_compare);
+
+			expect(out == expected_cmp);
+		} | Mats{};
+	}
+} // namespace
 
 int main()
 {
-	using namespace mpp;
-
 	feature("Type") = []() {
-		test_fn<matrix<int, 0, 0>, matrix_type>("utility/type/fixed.txt", mpp::type, stringify_type);
-		test_fn<matrix<int>, matrix_type>("utility/type/dyn.txt", mpp::type, stringify_type);
-		test_fn<matrix<int, dynamic, 0>, matrix_type>("utility/type/dyn_rows.txt", mpp::type, stringify_type);
-		test_fn<matrix<int, 0, dynamic>, matrix_type>("utility/type/dyn_cols.txt", mpp::type, stringify_type);
+		test_fn<fixed_mat<int, 0, 0>>("utilities/type/fixed.txt", mpp::type, parse_val<matrix_type>);
+		test_fn<dyn_mat<int>>("utilities/type/dyn.txt", mpp::type, parse_val<matrix_type>);
+		test_fn<dyn_rows_mat<int, 0>>("utilities/type/dyn_rows.txt", mpp::type, parse_val<matrix_type>);
+		test_fn<dyn_cols_mat<int, 0>>("utilities/type/dyn_cols.txt", mpp::type, parse_val<matrix_type>);
 	};
 
 	feature("Square") = []() {
-		test_fn<matrix<int, 1, 1>, bool>("utility/sq/1x1.txt", square, stringify_noop);
-		test_fn<matrix<int>, bool>("utility/sq/1x2.txt", square, stringify_noop);
-		test_fn<matrix<int, dynamic, 3>, bool>("utility/sq/3x3.txt", square, stringify_noop);
-		test_fn<matrix<int, 3, dynamic>, bool>("utility/sq/3x2.txt", square, stringify_noop);
+		test_fn<all_mats<int, 1, 1>>("utilities/sq/1x1.txt", square, parse_val<bool>);
+		test_fn<all_mats<int, 1, 2>>("utilities/sq/1x2.txt", square, parse_val<bool>);
+		test_fn<all_mats<int, 3, 3>>("utilities/sq/3x3.txt", square, parse_val<bool>);
+		test_fn<all_mats<int, 3, 2>>("utilities/sq/3x2.txt", square, parse_val<bool>);
 	};
 
 	feature("Singular") = []() {
-		test_fn<matrix<int, 0, 0>, bool>("utility/sg/0x0.txt", mpp::singular, stringify_noop);
-		test_fn<matrix<int>, bool>("utility/sg/1x1.txt", mpp::singular, stringify_noop);
-		test_fn<matrix<int, dynamic, 2>, bool>("utility/sg/2x2.txt", mpp::singular, stringify_noop);
-		test_fn<matrix<int, 3, dynamic>, bool>("utility/sg/3x3.txt", mpp::singular, stringify_noop);
+		test_fn<fixed_mat<int, 0, 0>>("utilities/sg/0x0.txt", singular, parse_val<bool>);
+		test_fn<dyn_mat<int>>("utilities/sg/1x1.txt", singular, parse_val<bool>);
+		test_fn<dyn_rows_mat<int, 2>>("utilities/sg/2x2.txt", singular, parse_val<bool>);
+		test_fn<dyn_cols_mat<int, 3>>("utilities/sg/3x3.txt", singular, parse_val<bool>);
 	};
 
 	feature("Size comparison") = []() {
-		test_cmp_size<matrix<int, 0, 0>, matrix<double, 0, dynamic>>("utility/cmp_size/0x0_0x0.txt");
-		test_cmp_size<matrix<int>, matrix<int, dynamic, 2>>("utility/cmp_size/1x1_1x2.txt");
-		test_cmp_size<matrix<int, dynamic, 4>, matrix<int>>("utility/cmp_size/3x4_3x3.txt");
-		test_cmp_size<matrix<int, 4, dynamic>, matrix<int, 4, 4>>("utility/cmp_size/4x4_4x4.txt");
+		test_cmp_size<join_mats<all_mats<int, 0, 0>, all_mats<int, 0, 0>>,
+			std::partial_ordering,
+			std::partial_ordering>("utilities/cmp_size/0x0_0x0.txt");
+		test_cmp_size<join_mats<all_mats<int, 1, 1>, all_mats<int, 1, 2>>, std::strong_ordering, std::strong_ordering>(
+			"utilities/cmp_size/1x1_1x2.txt");
+		test_cmp_size<join_mats<all_mats<int, 3, 4>, all_mats<int, 3, 3>>, std::partial_ordering, std::strong_ordering>(
+			"utilities/cmp_size/3x4_3x3.txt");
+		test_cmp_size<join_mats<all_mats<int, 4, 4>, all_mats<int, 4, 4>>, std::strong_ordering, std::strong_ordering>(
+			"utilities/cmp_size/4x4_4x4.txt");
 	};
 
-	feature("Elements compare") = []() {
-		test_cmp_elems<matrix<int, 0, 0>, matrix<double, 0, dynamic>, std::strong_ordering>(
-			"utility/cmp_elems/0x0_0x0.txt");
-		test_cmp_elems<matrix<int>, matrix<int, dynamic, 1>, std::strong_ordering>("utility/cmp_elems/1x1_1x1.txt");
-		test_cmp_elems<matrix<int, dynamic, 3>, matrix<int>, std::strong_ordering>("utility/cmp_elems/3x3_3x3.txt");
-		test_cmp_elems<matrix<double, 4, dynamic>, matrix<double, 4, 4>, std::partial_ordering>(
-			"utility/cmp_elems/4x4_4x4.txt");
+	feature("Elements comparison") = []() {
+		test_cmp_elems<join_mats<all_mats<int, 0, 0>, all_mats<int, 0, 0>>, std::strong_ordering>(
+			"utilities/cmp_elems/0x0_0x0.txt");
+		test_cmp_elems<join_mats<all_mats<int, 1, 1>, all_mats<int, 1, 1>>, std::strong_ordering>(
+			"utilities/cmp_elems/1x1_1x1.txt");
+		test_cmp_elems<join_mats<all_mats<int, 3, 3>, all_mats<int, 3, 3>>, std::strong_ordering>(
+			"utilities/cmp_elems/3x3_3x3.txt");
+		test_cmp_elems<join_mats<all_mats<float, 4, 4>, all_mats<float, 4, 4>>, std::partial_ordering>(
+			"utilities/cmp_elems/4x4_4x4.txt");
 	};
 
 	return 0;
